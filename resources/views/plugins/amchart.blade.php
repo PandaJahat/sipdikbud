64c0abf754464166caea905bc1061ff61fc50bc2
Abdurrohim Saifi <umum.saifi@gmail.com>
2019-06-21 17:02:33 +0700

    Development dashboard

---
+++ resources/views/plugins/amchart.blade.php
@@ -0,0 +1,5 @@
+@push('scripts')
+<script src="https://www.amcharts.com/lib/4/core.js"></script>
+<script src="https://www.amcharts.com/lib/4/charts.js"></script>
+<script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
+@endpush
---
+++ resources/views/plugins/peity.blade.php
@@ -0,0 +1,4 @@
+@push('scripts')
+    <script src="{{ asset('assets/bower_components/countUp.js/dist/countUp.min.js') }}"></script>
+    <script src="{{ asset('assets/bower_components/peity/jquery.peity.min.js') }}"></script>   
+@endpush
