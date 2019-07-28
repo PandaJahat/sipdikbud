ee1d2769c74e3884507908a6fe5fa0744d8758f7
Abdurrohim Saifi <umum.saifi@gmail.com>
2019-07-28 21:15:31 +0700

    Change collction API

--- app/Http/Controllers/API/CollectionController.php
+++ app/Http/Controllers/API/CollectionController.php
@@ -13,25 +13,33 @@ class CollectionController extends Controller
 {
     public function get(Request $request)
     {
-        $collections = Collection::where('is_active', true)->with([
-            'author', 'keywords', 
-        ])->get();
-
-        $data = [];
-
-        foreach ($collections as $key => $row) {
-            $data[] = [
-                'title' => $row->title,
-                'authors' => $row->author->name,
-                'subjects' => empty($row->subjects) ? '' : $row->subjects,
-                'keywords' => $row->keywords()->exists() ? implode(',', json_decode($row->keywords->pluck('keyword'))) : '',
-                'description' => empty($row->description) ? '' : $row->description,
-                'relations' => '',
-                'publisher' => empty($row->published_by) ? '' : $row->published_by,
-                'publishDate' => empty($row->published_year) ? '' : $row->published_year
+        try {
+            $collections = Collection::where('is_active', true)->with([
+                'author', 'keywords', 
+            ])->get();
+    
+            $data = [];
+    
+            foreach ($collections as $key => $row) {
+                $data[] = [
+                    'title' => $row->title,
+                    'authors' => $row->author->name,
+                    'subjects' => empty($row->subjects) ? '' : $row->subjects,
+                    'keywords' => $row->keywords()->exists() ? implode(',', json_decode($row->keywords->pluck('keyword'))) : '',
+                    'description' => empty($row->description) ? '' : $row->description,
+                    'relations' => '',
+                    'publisher' => empty($row->published_by) ? '' : $row->published_by,
+                    'publishDate' => empty($row->published_year) ? '' : $row->published_year
+                ];
+            }
+    
+            return [
+                'data' => $data
+            ];
+        } catch (\Exception $e) {
+            return [
+                'error' => $e->getMessage()
             ];
         }
-
-        return $data;
     }
 }
